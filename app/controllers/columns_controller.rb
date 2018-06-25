c91697c318c05950573d4a261bc584dfe6e192c8
Johannes Lohner <johannes.lohner@hotmail.com>
2018-06-25 21:13:13 +0200

    Add Column works

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -5,7 +5,14 @@ class ColumnsController < ApplicationController
   end
 
   def create
+    puts params
     @column = Column.new(params_column)
+    @column.user = current_user
+    @team_ids = params[:column][:team_ids].drop(1)
+    @team_ids.each do |team_id|
+      @team = Team.find(team_id.to_i)
+      @column.teams << @team
+    end
     authorize @column
     if @column.save
       redirect_to users_show_path
@@ -14,9 +21,16 @@ class ColumnsController < ApplicationController
     end
   end
 
+  def destroy
+    @column = Column.find(params[:id])
+    @column.destroy
+    authorize @column
+    redirect_to users_show_path
+  end
+
   private
 
   def params_column
-    params.require(:column).permit(:name, :description, :shared)
+    params.require(:column).permit(:name, :description, :shared, :teams)
   end
 end
--- app/policies/column_policy.rb
+++ app/policies/column_policy.rb
@@ -8,4 +8,16 @@ class ColumnPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
