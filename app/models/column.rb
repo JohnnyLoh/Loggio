03b59d2705d7ea846552d4fbdaf21c02570d6b5a
Johannes Lohner <johannes.lohner@hotmail.com>
2018-06-26 14:57:58 +0200

    Caard model added

--- app/models/column.rb
+++ app/models/column.rb
@@ -2,6 +2,7 @@ class Column < ApplicationRecord
   belongs_to :user
 
   has_many :columns
+  has_many :cards
 
   has_many :assigned_columns, dependent: :destroy
   has_many :teams, through: :assigned_columns
