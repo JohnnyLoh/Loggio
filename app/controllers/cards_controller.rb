594fd67d0d5353034c32ff06bdcaf692acccfa30
Johannes Lohner <johannes.lohner@hotmail.com>
2018-06-26 15:16:23 +0200

    added cards to columns view

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,10 @@
 class CardsController < ApplicationController
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    authorize @card
+    redirect_to cards_show_path
+  end
+
 end
---
+++ app/policies/card_policy.rb
@@ -0,0 +1,19 @@
+class CardPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
